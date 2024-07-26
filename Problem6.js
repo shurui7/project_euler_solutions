fba38021580d21f6478a9a08d83f2b5fe243be83
Shurui7 <96158419+Shurui7@users.noreply.github.com>
2024-07-26 14:41:51 -0400

    Added comments for the 6th problem

--- Problem6.js
+++ Problem6.js
@@ -1,20 +1,31 @@
-let arr_sum_of_sqrs = [];
-let i = 0;
-let b = 1;
-let sum_of_hundred = 0;
+// The sum of the squares of the first ten natural numbers is, 385
+// The square of the sum of the first ten natural numbers is, 3025
+// Hence the difference between the sum of the squares of the first
+// ten natural numbers and the square of the sum is 2640.
+//
+// Find the difference between the sum of the squares of the first
+// one hundred natural numbers and the square of the sum.
 
+
+// Variables
+let arr_sum_of_sqrs = []
+let i = 0
+let b = 1
+let sum_of_hundred = 0
+
+// Create an array with 100 squares 
 while (i < 100) {
-    arr_sum_of_sqrs.push(Math.sqrt(i));
-    i++
+	arr_sum_of_sqrs.push(Math.sqrt(i))
+	i++
 }
-
-let sum_of_sqrs = arr_sum_of_sqrs.reduce((a, b) => a + b, 0);
-
+// Sum them
+let sum_of_sqrs = arr_sum_of_sqrs.reduce((a, b) => a + b, 0)
+// Sum 100 natural numbers
 while (b < 100) {
-    sum_of_hundred = sum_of_hundred + b;
-    b++
+	sum_of_hundred = sum_of_hundred + b
+	b++
 }
-
-console.log(sum_of_sqrs - Math.sqrt(sum_of_hundred));
+// Display the result = (Sum of the squares - the square of 100 natural numbers)
+console.log(sum_of_sqrs - Math.sqrt(sum_of_hundred))
 
 // result 591.1067107057963
